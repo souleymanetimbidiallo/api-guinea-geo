7db6b6df74847aac24a2d3bdb35c06d8e5ddc8bc
Souleymane Diallo <souleymanetimbidiallo@gmail.com>
2025-04-01 22:27:21 +0200

    Fix: Swagger load for Vercel

--- swagger.js
+++ swagger.js
@@ -1,7 +1,10 @@
 const swaggerUi = require('swagger-ui-express');
+const path = require('path');
+const fs = require('fs');
 
-// On importe directement le JSON comme un module
-const swaggerDocument = require('./swagger.json'); // <== sans readFileSync
+const swaggerDocument = JSON.parse(
+  fs.readFileSync(path.join(__dirname, 'swagger.json'), 'utf8')
+);
 
 module.exports = (app) => {
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
