611ab2a96f24194eed580012b04c0ff896e34aec
Souleymane Diallo <souleymanetimbidiallo@gmail.com>
2025-04-01 22:30:55 +0200

    Fix swagger

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const errorHandler = require('./utils/errorHandler');
 require('dotenv').config();
-const swaggerDocs = require('./swagger');
+//const swaggerDocs = require('./swagger');
 
 
 const mongoose = require('mongoose');
@@ -10,7 +10,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
-swaggerDocs(app);
+//swaggerDocs(app);
 
 const regionRoutes = require('./routes/regionRoutes'); // Assurez-vous que le chemin est correct
 const communeRoutes = require('./routes/communeRoutes'); // Assurez-vous que le chemin est correct
